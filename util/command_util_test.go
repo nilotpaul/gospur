f92d3667fa7c3bdb42a1a6957f9d3df3edf02f38
nilotpaul <nilotpaul.nandi2021@uem.edu.in>
2024-11-21 15:00:26 +0530

    test: added unit test for validateGoModPath func

--- util/command_util_test.go
+++ util/command_util_test.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -45,4 +47,63 @@ func TestGetProjectPath(t *testing.T) {
 	a.Error(err)
 	a.ErrorContains(err, "Invalid directory path: '../new-project' contains '..'")
 	a.Nil(pp)
+}
+
+func TestValidateGoModPath(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+
+	// Given path is less than 3 character(s)
+	err := validateGoModPath("ww")
+	a.EqualError(err, "Path cannot be less than 3 character(s)")
+
+	// Given path contains https://
+	err = validateGoModPath("https://something")
+	a.EqualError(err, "Invalid path 'https://something', should not contain https")
+
+	// Given path contains a space
+	err = validateGoModPath("some thing")
+	a.EqualError(err, "Invalid path 'some thing', contains reserved characters")
+
+	// Given path contains a :
+	err = validateGoModPath("some:thing")
+	a.EqualError(err, "Invalid path 'some:thing', contains reserved characters")
+
+	// Given path contains *
+	err = validateGoModPath("some*thing")
+	a.EqualError(err, "Invalid path 'some*thing', contains reserved characters")
+
+	// Given path contains ?
+	err = validateGoModPath("github.com/paul?key=value")
+	a.EqualError(err, "Invalid path 'github.com/paul?key=value', contains reserved characters")
+
+	// Given path contains |
+	err = validateGoModPath("github.com/paul|repo")
+	a.EqualError(err, "Invalid path 'github.com/paul|repo', contains reserved characters")
+
+	// Given path exceedes 255 character(s)
+	err = validateGoModPath(generateRandomString(500))
+	a.EqualError(err, "Exceeded maximum length")
+
+	// Given path is valid
+	err = validateGoModPath("github.com/nilotpaul/gospur")
+	a.NoError(err)
+
+	// Given another path is valid
+	err = validateGoModPath("gospur")
+	a.NoError(err)
+}
+
+// Helper func only for testing
+//
+// GenerateRandomString generates a random string of a given length
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	rand.Seed(time.Now().UnixNano())
+
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(result)
 }
