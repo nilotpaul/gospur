4f510b2440bd42417f52af6a0852499308dac006
nilotpaul <nilotpaul.nandi2021@uem.edu.in>
2024-11-24 23:04:35 +0530

    fix: minor syntax issues in created stack

--- util/rawdata_util.go
+++ util/rawdata_util.go
@@ -10,32 +10,26 @@ import (
 const (
 	basicHomeBodyExampleHTML = `
 <body>
-  <main>
-    <div>
-      <h1>{{ index . "Ctx" "Title" }}</h1>
+  <main class="container">
+      <h1>{{ .Ctx.Title }}</h1>
       <img
         src="public/golang.jpg"
         class="rounded-md"
         height="500"
         width="500"
       />
-      <p>{{ index . "Ctx" "Desc" }}</p>
-    </div>
-
-    <div>
-      <button>Light</button>
-    </div>
+      <p>{{ .Ctx.Desc }}</p>
   </main>
 </body>`
 	tailwindHomeBodyExampleHTML = `
   <body
-    class="max-w-full min-h-screen bg-gray-300 dark:text-slate-200 text-black dark:bg-gray-950"
+    class="max-w-full bg-gray-300 dark:text-slate-200 text-black dark:bg-gray-950"
   >
     <!-- Container (MAX-WIDTH -> 48rem) -->
     <main class="max-w-3xl mx-auto">
       <div class="flex items-center gap-y-6 mt-4 flex-col justify-center">
-        <h1 class="text-4xl mt-6 mb-0 text-blue-600 font-bold">
-          {{ index . "Ctx" "Title" }}
+        <h1 class="text-4xl my-4 text-blue-600 font-bold">
+          {{ .Ctx.Title }}
         </h1>
         <img
           src="public/golang.jpg"
@@ -43,71 +37,18 @@ const (
           height="500"
           width="500"
         />
-        <p class="text-lg font-medium">{{ index . "Ctx" "Desc" }}</p>
-      </div>
-
-      <div class="flex items-center mt-4 justify-center gap-4">
-        <button
-          type="button"
-          class="hs-dark-mode hs-dark-mode-active:hidden inline-flex items-center gap-x-2 py-2 px-3 bg-black/80 rounded-full text-sm text-white hover:bg-black/70 focus:outline-none focus:bg-black/70"
-          data-hs-theme-click-value="dark"
-        >
-          <svg
-            class="shrink-0 size-4"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
-          </svg>
-          Dark
-        </button>
-        <button
-          type="button"
-          class="hs-dark-mode hs-dark-mode-active:inline-flex hidden items-center gap-x-2 py-2 px-3 bg-white/10 rounded-full text-sm text-white hover:bg-white/20 focus:outline-none focus:bg-white/20"
-          data-hs-theme-click-value="light"
-        >
-          <svg
-            class="shrink-0 size-4"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <circle cx="12" cy="12" r="4"></circle>
-            <path d="M12 2v2"></path>
-            <path d="M12 20v2"></path>
-            <path d="m4.93 4.93 1.41 1.41"></path>
-            <path d="m17.66 17.66 1.41 1.41"></path>
-            <path d="M2 12h2"></path>
-            <path d="M20 12h2"></path>
-            <path d="m6.34 17.66-1.41 1.41"></path>
-            <path d="m19.07 4.93-1.41 1.41"></path>
-          </svg>
-          Light
-        </button>
+        <p class="text-lg font-medium">{{ .Ctx.Desc }}</p>
       </div>
     </main>
   </body>  `
 
 	basicErrorBodyExampleHTML = `
-<body>
-  <h1>{{ index . "Ctx" "FullError" }}</h1>
+<body class="container">
+  <h1>{{ .Ctx.FullError }}</h1>
 </body>`
 	tailwindErrorBodyExampleHTML = `
 <body class="flex items-center justify-center">
-    <h1 class="font-bold">{{ index . "Ctx" "FullError" }}</h1>
+    <h1 class="font-bold">{{ .Ctx.FullError }}</h1>
 </body>`
 )
 
@@ -134,12 +75,12 @@ func processRawHomePageData(cfg StackConfig) string {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     %s
     <!-- For live reloading -->
-    {{ if index .IsDev }}
+    {{ if .IsDev }}
     <script src="http://localhost:35729/livereload.js"></script>
     {{ end }}
     %s
 
-    <title>{{ index . "Ctx" "Title" }}</title>
+    <title>{{ .Ctx.Title }}</title>
   </head>
   %s
 </html>
@@ -166,7 +107,7 @@ func processRawErrorPageData(cfg StackConfig) string {
     <script src="http://localhost:35729/livereload.js"></script>
     {{ end }}
 
-    <title>{{ index . "Ctx" "Msg" }}</title>
+    <title>{{ .Ctx.Msg }}</title>
   </head>
   %s
 </html>
@@ -210,13 +151,9 @@ func generateHeadScripts(cfg StackConfig) string {
 }
 
 func generateHeadStyles(cfg StackConfig) string {
-	styles := []string{"<!-- Styles -->"}
-
-	if cfg.CssStrategy == "Tailwind" {
-		styles = append(styles, `<link rel="stylesheet" href="public/bundle/styles.css" />`)
-	}
-	if len(styles) == 1 {
-		return ""
+	styles := []string{
+		"<!-- Styles -->",
+		`<link rel="stylesheet" href="public/bundle/globals.css" />`,
 	}
 
 	return strings.Join(styles, "\n")
