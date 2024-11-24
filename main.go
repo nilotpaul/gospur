ee022617700ffe7e59289fb03ee1071b8e917851
nilotpaul <nilotpaul.nandi2021@uem.edu.in>
2024-11-24 23:05:19 +0530

    feat: removed the early stage env setting

--- main.go
+++ main.go
@@ -8,13 +8,7 @@ import (
 )
 
 func main() {
-	// This sets a `EARLY_STAGE` env which we use to
-	// initialize a default project without any options.
-	// This is done as currently we don't have any extra
-	// options other than the default template.
-	os.Setenv("EARLY_STAGE", "True")
-
-	// Stars the CLI
+	// Starts the CLI
 	if err := Execute(); err != nil {
 		fmt.Println(config.NormalMsg("Go Spur exited"))
 		os.Exit(1)
